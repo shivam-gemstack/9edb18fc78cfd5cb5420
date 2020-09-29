eca5468bedef966baa6e05e45f0013a1897eb15e
Shivam Singh <shivam@gemstack.in>
2020-09-30 00:18:05 +0530

    updated code

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,5 @@
 class Api::UsersController < ApplicationController
-	REGEX_PATTERN = /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/ 
-
 	before_action :get_user, only: [:update, :show, :destroy]
-	before_action :check_email_valid?, only: [:create,:update]
 
 	def index
 		all_users = User.all
@@ -10,34 +7,21 @@ class Api::UsersController < ApplicationController
 		page = params[:page]||User::PAGE
 		per_page = params[:per_page]||User::PER_PAGE
 		users = all_users.paginate(page:page, per_page: per_page)		
-		if users.present?
-			mod = all_users.length % per_page.to_i
-			pages = all_users.length / per_page.to_i
-			pages+=1 if mod>0
-		else
-			pages = 0
-		end
-
+		pages = all_users.length / per_page.to_i
 		render json:{users: ActiveModel::SerializableResource.new(users,each_serializer:UsersSerializer).as_json,
 		meta:{
 			pagination:{
 				current_page: users.current_page,
-				total_pages: pages.present? ? pages: "",
+				total_pages:  pages,
 				per_page: per_page,
-				total_count: total_count
+				total_users_count: total_count
 			}
 		} }
 	end
 
 	def create
 		user = User.create!(user_params)
-		if user.save!
-			render json:{user: UsersSerializer.new(user).serializable_hash}
-		else
-			render json: {
-				errror:"Unable to create a user"
-			}
-		end
+		render json:{user: UsersSerializer.new(user).serializable_hash}
 	end
 
 	def show
@@ -68,19 +52,6 @@ class Api::UsersController < ApplicationController
 	def get_user
 		@user  = User.find(params[:id])
 	end
-
-	def check_email_valid?
-		if is_email_valid? params[:email]
-			true
-		else
-			return render json:{error:"email format is not valid"}
-		end
-	end
-
-	def is_email_valid? email
-    email =~REGEX_PATTERN 
-	end
-
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
 	validates_uniqueness_of :email
+	validates_format_of :email, with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
 	PAGE =1
 	PER_PAGE=3
+
 
 	def self.typehead(input)
 		where("email like ? OR firstName like ? OR lastName like ?", "#{input}%","#{input}%","#{input}%")
